0f54f13bd1ea0347acd7e0db20d4d8d2df817be0
Ooke Nivalainen <ooke.nivalainen@gmail.com>
2020-02-12 16:45:39 +0200

    Added delete functionality to booklist

--- src/main/java/com/example/Bookstore/web/BookController.java
+++ src/main/java/com/example/Bookstore/web/BookController.java
@@ -3,6 +3,7 @@ package com.example.Bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -35,6 +36,12 @@ public class BookController {
 		repository.save(book);
 		
 		return "redirect:booklist";
+	}
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		
+		return"redirect:../booklist";
 	}
 	
 	}
