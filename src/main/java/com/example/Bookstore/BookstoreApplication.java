3fc849f05f40106e41f16307c8bbd17cdba97ce1
Ooke Nivalainen <ooke.nivalainen@gmail.com>
2020-04-08 16:46:47 +0300

    changed users to kayttaja

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -39,12 +39,12 @@ public class BookstoreApplication {
 			repository.save(book3);
 			
 			// Create users: admin/admin user/user
-						Kayttaja user1 = new Kayttaja("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
-						Kayttaja user2 = new Kayttaja("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
-						Kayttaja user3 = new Kayttaja("Ooke", "$2y$10$3N9WOt3Z0s1/dx50sHhE4O16Q9O41Xgr6E.2sT55NIWrMJg0qsWGi", "ADMIN");
-						urepository.save(user1);
-						urepository.save(user2);
-						urepository.save(user3);
+						Kayttaja kayttaja1 = new Kayttaja("käyttäjä", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "KAYTTAJA");
+						Kayttaja kayttaja2 = new Kayttaja("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+						Kayttaja kayttaja3 = new Kayttaja("Ooke", "$2y$10$3N9WOt3Z0s1/dx50sHhE4O16Q9O41Xgr6E.2sT55NIWrMJg0qsWGi", "ADMIN");
+						urepository.save(kayttaja1);
+						urepository.save(kayttaja2);
+						urepository.save(kayttaja3);
 			
 			};
 	}
